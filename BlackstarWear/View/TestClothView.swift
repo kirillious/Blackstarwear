ab3b53620c55c7fcec7837377174b6084f716127
kirillious <kirill12345@hotmail.com>
2021-07-28 01:36:08 +0300

    Load indicator adds, empty cells remove, more safety while parsing adds, scrollView changes

---
+++ BlackstarWear/View/TestClothView.swift
@@ -0,0 +1,20 @@
+//
+//  TestClothView.swift
+//  BlackstarWear
+//
+//  Created by Kirill on 25.07.2021.
+//
+
+import UIKit
+
+class TestClothView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
